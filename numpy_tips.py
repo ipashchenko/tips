ac8db19cae574b01e80c3cc86c22f881729daa42
Ilya <in4pashchenko@gmail.com>
2015-01-24 19:22:18 +0300

    added docstrings

--- numpy_tips.py
+++ numpy_tips.py
@@ -3,6 +3,13 @@ def fields_view(arr, fields):
     How to return a view of several columns in numpy structured array?
     http://stackoverflow.com/questions/15182381/how-to-return-a-view-of-several-columns-in-numpy-structured-array
     Answered by http://stackoverflow.com/users/772649/hyry
+    
+    :param arr:
+        Numpy structured array.
+    :param fields:
+        Iterable of fileds for view to return.
+    :return:
+        View to original array with only fields listed in ``fields``.
     """
     dtype2 = np.dtype({name:arr.dtype.fields[name] for name in fields})
     return np.ndarray(arr.shape, dtype2, arr, 0, arr.strides)
